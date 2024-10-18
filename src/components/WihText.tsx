2b264e66458167346f9a2a54ef02642ee131b2f9
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-18 15:11:50 +0200

    title

--- src/components/WihText.tsx
+++ src/components/WihText.tsx
@@ -1,5 +1,5 @@
 import { useThemeColor } from "@/hooks/useThemeColor";
-import { Text, TextProps } from "react-native";
+import { Text, TextProps, TextStyle } from "react-native";
 
 export const WihText = ({ children, style, ...rest }: TextProps) => {
     const color = useThemeColor("text");
@@ -7,4 +7,18 @@ export const WihText = ({ children, style, ...rest }: TextProps) => {
     return (
         <Text style={[{ color }, style]} {...rest}>{children}</Text>
     );
+}
+
+export const WihTitle = ({ children, style, ...rest }: TextProps) => {
+    const color = useThemeColor("text");
+    const titleStyle: TextStyle = {
+        marginTop: 10,
+        fontWeight: "bold",
+        fontSize: 20,
+        color
+    }
+
+    return (
+        <Text style={[titleStyle, style]} {...rest}>{children}</Text>
+    )
 }
