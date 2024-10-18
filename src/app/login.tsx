eab1564ecb9be1ba68f991a3b502291f5d12e6e0
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-10-18 15:44:08 +0200

    Create login.tsx

---
+++ src/app/login.tsx
@@ -0,0 +1,14 @@
+import { WihTitle } from "@/components/WihText";
+import WihView from "@/components/WihView";
+
+// https://docs.expo.dev/router/reference/authentication/
+
+const Login = () => {
+    return (
+        <WihView center="horizontal">
+            <WihTitle>Login</WihTitle>
+        </WihView>
+    )
+}
+
+export default Login;
