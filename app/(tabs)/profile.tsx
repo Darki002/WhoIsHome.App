9e8f3dcec5c96f8293dfdf6ed013f830b8ff898c
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-11-03 15:14:24 +0100

    fetch user fro profile view

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,32 +1,48 @@
-import { useSession } from "@/components/auth/context";
+import {Tokens, useSession} from "@/components/auth/context";
 import { WihAvatar } from "@/components/WihAvatar";
 import { WihButton } from "@/components/WihButton";
 import { WihText, WihTitle } from "@/components/WihText";
 import WihView from "@/components/WihView";
 import { Dimensions, StyleSheet, ViewStyle } from 'react-native';
+import {TokensProps, wihFetch} from "@/components/api/whoIsHomeApi";
 
-const Profile = () => {
-    const { signOut } = useSession();
+type User = {
+    Id: number;
+    UserName: string;
+    Email: string;
+}
+
+const Profile = async () => {
+    const { signOut, session } = useSession();
+
+    if(!session) {
+        return <WihTitle>Oops, Error occurred...</WihTitle>
+    }
 
     const dim = Dimensions.get("screen");
     const viewStyle: ViewStyle = {
         marginLeft: dim.width / 12,
         marginTop: dim.height / 16
     }
-
     const avatarStyle = {
         marginRight: dim.width / 20
     }
-
     const textStyle = {
         fontSize: dim.fontScale * 24
     }
 
+    const userResponse = await getUser(session);
+
+    if(userResponse.hasError) {
+        return <WihTitle>Oops, Error occurred...</WihTitle>
+    }
+
+    const userName = userResponse.response!.UserName
     return (
         <>
             <WihView style={[viewStyle, styles.view]}>
-                <WihAvatar name="Darki" size={dim.scale * 14} style={avatarStyle} />
-                <WihText style={[styles.text, textStyle]}>Darki</WihText>
+                <WihAvatar name={userName} size={dim.scale * 14} style={avatarStyle} />
+                <WihText style={[styles.text, textStyle]}>{userName}</WihText>
                 <WihButton onPress={() => signOut()}>Logout</WihButton>
             </WihView >
             <WihView center="horizontal">
@@ -34,10 +50,23 @@ const Profile = () => {
                 {/* Collabsalbes for Today, this week, future */}
                 {/* OR all Events listed in albathatic order */}
                 {/* OR custom sort: name, date, created */}
-                <WihText>laoding...</WihText>
+                <WihText>Loading...</WihText>
             </WihView>
         </>
     );
+}
+
+async function getUser(session : Tokens) {
+    const response = await wihFetch<User>({
+        endpoint: "Auth/Me",
+        method: "GET",
+        tokens: {
+            Token: session.jwtToken!,
+            RefreshToken: session.refreshToken!
+        }
+    });
+
+    return response;
 }
 
 const styles = StyleSheet.create({
