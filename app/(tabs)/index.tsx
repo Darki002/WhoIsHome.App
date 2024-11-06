0700c8062971d8e0209cb03d4dfb0d50cb2cba34
Llyn Baumann <124689003+LlynBaum@users.noreply.github.com>
2024-11-06 14:22:47 +0100

    prep request

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,22 @@
 import { WihText } from "@/components/WihText";
 import WihView from "@/components/WihView";
+import useWihApiInterval from "@/hooks/useWihApiInterval";
+
+const TIME = 5 * 60 * 1000;
+
+type DailyOverview = {
+    userId: number,
+    isAtHome: boolean,
+    dinnerTime: "string"
+}
 
 const Index = () => {
+    const response = useWihApiInterval<DailyOverview>({
+        time: TIME,
+        method: "GET",
+        endpoint: "DailyOverview"
+    });
+
   return (
     <WihView center="full">
       <WihText>Hello Home!</WihText>
