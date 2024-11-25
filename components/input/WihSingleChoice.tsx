8fe521967716add6b86fe685bb9e9f40d15bf1d8
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-11-25 20:07:33 +0100

    Create WihSingleChoice.tsx

---
+++ components/input/WihSingleChoice.tsx
@@ -0,0 +1,47 @@
+import {View} from "react-native";
+import {useThemeColor} from "@/hooks/useThemeColor";
+import {WihButton} from "@/components/WihButton";
+
+export type WihOption<T> = {
+    value: T | null;
+    display: string;
+}
+
+export interface WihSingleChoiceProps<T> {
+    value: T | null;
+    options: Array<WihOption<T>>;
+    onChange: (value: T | null) => void;
+}
+
+export function WihSingleChoice<T>({value, options, onChange} : WihSingleChoiceProps<T>) {
+    const backgroundColor = useThemeColor('background');
+    return (
+        <View style={{backgroundColor, flex: 2}}>
+            {options.map(o =>
+                    <OptionButton
+                        value={o.value}
+                        display={o.display}
+                        isSelected={o.value == value}
+                        onChange={onChange} />
+                )}
+        </View>
+    );
+}
+
+interface OptionButtonProps<T> {
+    value: T | null;
+    display: string;
+    isSelected: boolean;
+    onChange: (value: T | null) => void;
+}
+
+function OptionButton<T>({value, display, isSelected, onChange} :OptionButtonProps<T>){
+    const primary = useThemeColor('primary');
+    const secondary = useThemeColor('secondary');
+
+    const backgroundColor = isSelected ? primary : secondary;
+
+    return (
+        <WihButton onPress={() => onChange(value)} style={{backgroundColor}}>{display}</WihButton>
+    );
+}
