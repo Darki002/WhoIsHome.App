265a7427ffad2cfa8d2febb341b8f728852618b3
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-01-10 22:18:23 +0100

    button

--- components/input/WihButton.tsx
+++ components/input/WihButton.tsx
@@ -1,30 +1,52 @@
-import {Pressable, StyleSheet, Text, ViewStyle} from "react-native";
-import {ReactNode} from "react";
+import {Pressable, StyleSheet, Text, TextStyle, ViewStyle} from "react-native";
+import {FC, ReactNode} from "react";
 import {useWihTheme} from "@/components/WihThemeProvider";
 
 type WihButtonProps = {
     children: ReactNode;
     onPress: () => void;
-    style?: object;
+    disabled?: boolean;
+    style?: ViewStyle;
+    textStyle?: TextStyle;
 };
 
-export const WihButton = ({children, onPress, style}: WihButtonProps) => {
+export const WihButton: FC<WihButtonProps> = ({
+                                                        children,
+                                                        onPress,
+                                                        disabled = false,
+                                                        style,
+                                                        textStyle
+                                                    }) => {
     const theme = useWihTheme();
 
-    const buttonStyle: ViewStyle = {
-        backgroundColor: theme.background
-    }
-
     return (
-        <Pressable onPress={onPress} style={[buttonStyle, styles.button, style]}>
-            <Text>{children}</Text>
+        <Pressable
+            onPress={onPress}
+            disabled={disabled}
+            style={({ pressed }) => [
+                styles.button,
+                { backgroundColor: disabled ? theme.disabled : theme.primary },
+                pressed && !disabled && { backgroundColor: theme.primary },
+                style
+            ]}
+        >
+            <Text style={[styles.text, { color: theme.buttonText }, textStyle]}>
+                {children}
+            </Text>
         </Pressable>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     button: {
-        padding: 8,
-        borderRadius: 7
-    }
+        paddingVertical: 10,
+        paddingHorizontal: 15,
+        borderRadius: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
 });
