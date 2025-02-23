55eddb5feddcce193a2a690b52d44acecc398de5
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-02-23 13:42:44 +0100

    ka

--- hooks/usePermission.ts
+++ hooks/usePermission.ts
@@ -6,7 +6,6 @@ export function usePermission(){
     const [user] = useWihApi<User>({endpoint: "User/Me", method: "GET"});
 
     return useCallback((permittedUserId?: number) => {
-        console.log(`Permission check for ${permittedUserId} with current user ${user?.response?.id}`);
         if(!user?.response) return false;
         if(!permittedUserId) return false;
         return permittedUserId === user?.response?.id;
