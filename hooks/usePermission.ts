ba792e6b244bb796308bd02d3edaca21f3991711
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2025-03-01 18:46:34 +0100

    Update usePermission.ts

--- hooks/usePermission.ts
+++ hooks/usePermission.ts
@@ -1,13 +1,20 @@
 import {useCallback} from "react";
 import {User} from "@/constants/WihTypes/User";
 import useWihApi from "@/hooks/wihApi/useWihApi";
+import {WihLogger} from "@/helper/WihLogger";
 
 export function usePermission(){
     const [user] = useWihApi<User>({endpoint: "User/Me", method: "GET"});
 
     return useCallback((permittedUserId?: number) => {
-        if(!user?.data) return false;
-        if(!permittedUserId) return false;
+        if(!user?.data) {
+            WihLogger.warn("(Permission Check) Logged in user was not being loaded.");
+            return false;
+        }
+        if(!permittedUserId) {
+            WihLogger.warn("(Permission Check) There was no given user ID that can access this resource.");
+            return false;
+        }
         return permittedUserId === user.data.id;
     }, [user]);
 }
