59fae8c52b187637ffc9ba882363ee2677c08e99
Darki002 <113591988+Darki002@users.noreply.github.com>
2025-05-03 19:39:54 +0200

    Update DailyOverview.ts

--- constants/WihTypes/DailyOverview.ts
+++ constants/WihTypes/DailyOverview.ts
@@ -1,13 +1,24 @@
-import {SimpleUser} from "@/constants/WihTypes/User";
+import {timeStringToDate} from "@/helper/datetimehelper";
 
-export interface DailyOverview {
-    isAtHome: boolean;
-    dinnerTime: string | null;  // "HH:mm:ss" or null
+export type DailyOverviewUser = {
+    id: number;
+    username: string;
 }
 
-export type DailyOverviews = Record<string, DailyOverview>;
+export interface DailyOverviewDto {
+    user: DailyOverviewUser;
+    isAtHome: boolean;
+    dinnerTime: string | null;
+}
 
-export interface WeeklyReport {
-    user: SimpleUser;
-    dailyOverviews: DailyOverviews;
+export class DailyOverview {
+    User: DailyOverviewUser;
+    IsAtHome: boolean;
+    DinnerTime: Date | null;
+
+    constructor(dailyOverviewDto : DailyOverviewDto) {
+        this.User = dailyOverviewDto.user;
+        this.IsAtHome = dailyOverviewDto.isAtHome;
+        this.DinnerTime = timeStringToDate(dailyOverviewDto.dinnerTime ?? undefined) ?? null;
+    }
 }
